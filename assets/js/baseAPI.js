d9a105de4419357c7d72b6cd0ea99670871f53f0
lh <765829624@qq.com>
2020-11-26 10:36:24 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,26 @@
-var baseURL='http://ajax.frontend.itheima.net'
+var baseURL = 'http://ajax.frontend.itheima.net'
 $.ajaxPrefilter(function (params) {
-    //拼接对应环境的服务器地址
-    params.url=baseURL+params.url
+    //1.拼接对应环境的服务器地址
+    params.url = baseURL + params.url
+    //2.身份认证
+    //对需要权限的接口配制头信息
+    //必须以my开头
+    if (params.url.indexOf('/my/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //3.登录拦截
+    params.complete = function (res) {
+        var obj = res.responseJSON
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            //清空本地存储
+            localStorage.removeItem('token')
+            //跳转页面
+            location.href='/login.html'
+        }
+    }
+
+
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,19 @@
 $(function () {
     //1.获取用户信息
     getuserInof() 
+    //2.退出功能
+    var layer = layui.layer
+    $('#btnLogout').on('click', function () {
+        //框架提供的询问框
+        layer.confirm('是否确认退出?', {icon: 3, title:'提示'}, function(index){
+           //清除本地存储'token'
+            localStorage.removeItem('token')
+            //跳转页面
+            location.href = '/login.html'
+            //关闭询问框
+            layer.close(index)
+          });
+    })
 })
 //获取用户信息(封装到入口函数外面)
 //后面函数要调用
@@ -9,10 +22,10 @@ function getuserInof() {
     //发送ajax
     $.ajax({
         url: '/my/userinfo',
-        headers: {
-            //重新登录,因为token过期事件12小时
-            Authorization:localStorage.getItem('token') || ''
-        },
+        // headers: {
+        //     //重新登录,因为token过期事件12小时
+        //     Authorization:localStorage.getItem('token') || ''
+        // },
         success: function (res) {
             console.log(res);
             //判断状态码
@@ -24,7 +37,7 @@ function getuserInof() {
         }
     })
 }
-//
+//渲染用户头像
 function renderAvatar(user) {
     //1.用户名(昵称优先,没有用username)
     var name = user.nickname || user.username
